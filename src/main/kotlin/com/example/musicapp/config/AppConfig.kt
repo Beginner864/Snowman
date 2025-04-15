52f586e740815119eae4b26c424ebbd15f903af8
눈사람 <noonma777@gmail.com>
2025-04-15 11:09:54 +0900

    아

--- src/main/kotlin/com/example/musicapp/config/AppConfig.kt
+++ src/main/kotlin/com/example/musicapp/config/AppConfig.kt
@@ -3,6 +3,8 @@ package com.example.musicapp.config
 import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.Environment
 
 @Configuration
 class AppConfig {
@@ -12,6 +14,8 @@ class AppConfig {
         val dotenv = Dotenv.configure().load()
         return dotenv["OPENAI_API_KEY"] ?: throw IllegalArgumentException("API key not found")
     }
+
+
 }
 
 
--- src/main/kotlin/com/example/musicapp/model/Song.kt
+++ src/main/kotlin/com/example/musicapp/model/Song.kt
@@ -1,14 +1,17 @@
 package com.example.musicapp.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
+
 @Entity
 data class Song(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name = "song_id", columnDefinition = "serial")
     val id: Long? = null,  // 노래 고유 ID
     var title: String,     // 노래 제목
     var artist: String,    // 아티스트 이름
@@ -16,4 +19,6 @@ data class Song(
     var mood: String,       // 기분
     var streamingUrl: String // url
 )
+
+
 
