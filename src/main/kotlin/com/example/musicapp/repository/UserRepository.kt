105d8011171dba15c791a6d1d85bf53b2f7200bc
눈사람 <noonma777@gmail.com>
2025-05-08 23:46:24 +0900

    수정,추가 (제발..)

--- src/main/kotlin/com/example/musicapp/repository/UserRepository.kt
+++ src/main/kotlin/com/example/musicapp/repository/UserRepository.kt
@@ -3,9 +3,13 @@ package com.example.musicapp.repository
 import com.example.musicapp.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
     // username을 기준으로 User를 조회하는 메서드 정의
     fun findByUsername(username: String): User?
+
+    // ID를 기준으로 User를 조회하는 메서드를 명시적으로 추가
+    override fun findById(id: Long): Optional<User>
 }
